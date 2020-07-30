a6fadd827b32acce5db307d629cac7240c10c799
Aaron Graham <aaron.graham@unb.ca>
2020-07-30 18:19:55 -0300

    Check for JDK Directory and 'time' Utility Before Running; Disable VIM3 A53 Cores
    Signed-off-by: Aaron Graham <aaron.graham@unb.ca>

--- run_all.sh
+++ run_all.sh
@@ -2,12 +2,37 @@
 
 JAVA_ROOT=/opt/jdk
 
+# Make Sure an /opt/jdk*/*/bin/java Exists:
+if [[ ! -d /opt/jdk*/ ]];
+then
+	echo "${0##*/}@${HOSTNAME}: ERROR: The '/opt/jdk*/' Directory Doesn't Exist!"
+	exit -1
+fi
+
+# Make Sure an /opt/jdk*/*/bin/java Exists:
+if [[ ! -a /opt/jdk*/*/bin/java ]];
+then
+	echo "${0##*/}@${HOSTNAME}: ERROR: An '/opt/jdk*/*/bin/java' Executable Doesn't Exist!"
+	exit -1
+fi
+
+# Check if '/usr/bin/time' Exists:
+if [[ ! -a /usr/bin/time ]];
+then
+	echo "${0##*/}@${HOSTNAME}: ERROR: The '/usr/bin/time' Executable Doesn't Exist!"
+	exit -1
+fi
+
 case $(uname -m) in
     aarch64)
 		case $(uname -n) in
 			Khadas)
-				# vim3 is at 1.40
-				sudo ./lock_frequency.sh "1.40"
+				# vim3 has 6 cpus, we need to disable 0 and 1 since they are A53 cores
+				echo 0 | sudo tee /sys/devices/system/cpu/cpu0/online
+				echo 0 | sudo tee /sys/devices/system/cpu/cpu1/online
+
+				# vim3 is at 1.30
+				sudo ./lock_frequency.sh "1.30"
 				CPU_TYPE="A73"
 				;;
 			*)
@@ -21,10 +46,10 @@ case $(uname -m) in
 		# atom board closest frequency is 1.28
 		sudo ./lock_frequency.sh "1.28"
 		CPU_TYPE="amd64"
-        ;;
+		;;
 esac
 
-ITTERATE="1"
+ITERATE="1"
 BENCHMARK_DIR=(
 	"Benchmarks/SPECjvm2008"
 	"Benchmarks/DaCapo"
@@ -54,7 +79,7 @@ do
 				;;
 			esac
 
-			for (( i=1; i <= ITTERATE; i++ ))
+			for (( i=1; i <= ITERATE; i++ ))
 			do
 				make CPU_TYPE="${CPU_TYPE}" VM="${VM}" JAVA_HOME="${JAVA_HOME}" JAVA_OPTS="${JAVA_OPTS}"
 			done
