d90ccf9862a5d573664d493778c0a4228f0d9b34
Aaron Graham <aaron.graham@unb.ca>
2020-07-31 16:29:57 -0300

    Improve JDK Detection Support; Modify Sleep Location and Duration Between Sub-Benchmark Runs
    Signed-off-by: Aaron Graham <aaron.graham@unb.ca>

--- run_all.sh
+++ run_all.sh
@@ -1,25 +1,48 @@
 #!/bin/bash
 
-JAVA_ROOT=/opt/jdk
+echo -e "\n${0##*/}@${HOSTNAME}:$( date "+%Y.%m.%d.%H%M.%S" ): Init:"
 
-# Make Sure an /opt/jdk*/*/bin/java Exists:
-if [[ ! -d /opt/jdk*/ ]];
+# Set JAVA_ROOT from Command Line Argument:
+if [[ "" != $1 ]] && [[ -a "$1/bin/java" ]];
 then
-	echo "${0##*/}@${HOSTNAME}: ERROR: The '/opt/jdk*/' Directory Doesn't Exist!"
+	echo -e "\n${0##*/}@${HOSTNAME}:$( date "+%Y.%m.%d.%H%M.%S" ): INFO: Setting JDK from Command-Line Argument (\$1: '$1').\n"
+	JAVA_ROOT=$1
+
+# Make Sure an /opt/jdk*/ Directory Exists and an /opt/jdk*/*/bin/java Executable Exists:
+elif [[ -d /opt/jdk*/ ]] && [[ -a /opt/jdk*/*/bin/java ]];
+then
+	echo -e "\n${0##*/}@${HOSTNAME}:$( date "+%Y.%m.%d.%H%M.%S" ): WARNING: Setting JDK from Newest Found JDK in /opt/jdk*/ Directory (ls -altr /opt/jdk*/*/bin/java | tail -n1 | sed -e 's#/bin/java\$##': '`ls -altr /opt/jdk*/*/bin/java | tail -n | sed -e 's#/bin/java$##'`').\n"
+	JAVA_ROOT=`ls -altr /opt/jdk*/*/bin/java | tail -n1 | sed -e 's#/bin/java$##'`
+
+# Get JDK from JDK_HOME
+elif [[ "" != "$JDK_HOME" ]] && [[ -a "$JDK_HOME/bin/java" ]];
+then
+	echo -e "\n${0##*/}@${HOSTNAME}:$( date "+%Y.%m.%d.%H%M.%S" ): WARNING: Setting JDK from JDK_HOME (JDK_HOME: '$JDK_HOME').\n"
+	JAVA_ROOT="$JDK_HOME"
+
+# Get JDK from JAVA_HOME
+elif [[ "" != "$JAVA_HOME" ]] && [[ -a "$JAVA_HOME/bin/java" ]];
+then
+	echo -e "\n${0##*/}@${HOSTNAME}:$( date "+%Y.%m.%d.%H%M.%S" ): WARNING: Setting JDK from JAVA_HOME (JAVA_HOME: '$JAVA_HOME').\n"
+	JAVA_ROOT="$JAVA_HOME"
+
+# Get JDK from PATH
+elif [[ "" != "$PATH" ]] && [[ "" != "`which java`" ]];
+then
+	echo -e "\n${0##*/}@${HOSTNAME}:$( date "+%Y.%m.%d.%H%M.%S" ): WARNING: Setting JDK from PATH (which java | sed -e 's#/bin/java\$##': '`which java | sed -e 's#/bin/java$##'`').\n"
+	JAVA_ROOT=`which java | sed -e 's#/bin/java$##'`
+
+else
+	echo -e "\n${0##*/}@${HOSTNAME}:$( date "+%Y.%m.%d.%H%M.%S" ): ERROR: No Passed in JDK and Couldn't find a valid JDK in JDK_HOME, JAVA_HOME or PATH to Execute!\n\nExiting...\n"
 	exit -1
 fi
 
-# Make Sure an /opt/jdk*/*/bin/java Exists:
-if [[ ! -a /opt/jdk*/*/bin/java ]];
-then
-	echo "${0##*/}@${HOSTNAME}: ERROR: An '/opt/jdk*/*/bin/java' Executable Doesn't Exist!"
-	exit -1
-fi
+echo -e "${0##*/}@${HOSTNAME}:$( date "+%Y.%m.%d.%H%M.%S" ): JAVA_ROOT: '$JAVA_ROOT'"
 
-# Check if '/usr/bin/time' Exists:
+# Check if The '/usr/bin/time' Executable Exists:
 if [[ ! -a /usr/bin/time ]];
 then
-	echo "${0##*/}@${HOSTNAME}: ERROR: The '/usr/bin/time' Executable Doesn't Exist!"
+	echo -e "\n${0##*/}@${HOSTNAME}:$( date "+%Y.%m.%d.%H%M.%S" ): ERROR: The '/usr/bin/time' Executable Doesn't Exist!\n\nExiting...\n"
 	exit -1
 fi
 
@@ -42,49 +65,74 @@ case $(uname -m) in
 				;;
 		esac
         ;;
-    *)
-		# atom board closest frequency is 1.28
-		sudo ./lock_frequency.sh "1.28"
-		CPU_TYPE="amd64"
-		;;
+    x86_64)
+		case $(uname -n) in
+			CASA40)
+				CPU_TYPE="amd64"
+				;;
+			*)
+				# atom board closest frequency is 1.28
+				sudo ./lock_frequency.sh "1.28"
+				CPU_TYPE="amd64"
+				;;
+		esac
+        ;;
+	*)
+		echo -e "\n${0##*/}@${HOSTNAME}:$( date "+%Y.%m.%d.%H%M.%S" ): ERROR: Architecture '$(uname -m)' Not Supported!\n\nExiting...\n"
+		exit -1
+        ;;
 esac
 
 ITERATE="1"
 BENCHMARK_DIR=(
-	"Benchmarks/SPECjvm2008"
-	"Benchmarks/DaCapo"
-	"Benchmarks/ScimarkC"
-	"Benchmarks/ScimarkJava"
+#	"Benchmarks/SPECjvm2008"
+#	"Benchmarks/DaCapo"
+#	"Benchmarks/ScimarkC"
+#	"Benchmarks/ScimarkJava"
 	"Benchmarks/RenaissanceSuite"
 )
+
+JAVA_HOME="${JAVA_ROOT}"
+JAVA_OPTS=" -Xms2G -Xmx2G "
+VM="$(basename "${JAVA_ROOT}")"
+
+echo -e "${0##*/}@${HOSTNAME}:$( date "+%Y.%m.%d.%H%M.%S" ): BENCHMARK_DIR[@]: '${BENCHMARK_DIR[@]}'"
+echo -e "${0##*/}@${HOSTNAME}:$( date "+%Y.%m.%d.%H%M.%S" ): JAVA_HOME: '$JAVA_HOME'"
+echo -e "${0##*/}@${HOSTNAME}:$( date "+%Y.%m.%d.%H%M.%S" ): JAVA_OPTS: '$JAVA_OPTS'"
+echo -e "${0##*/}@${HOSTNAME}:$( date "+%Y.%m.%d.%H%M.%S" ): VM: '$VM'\n"
 
 for BENCHMARK in ${BENCHMARK_DIR[@]}
 do
 	pushd "${BENCHMARK}" || exit 1
-	for jdk_v in "${JAVA_ROOT}"*
+
+	case "${VM}" in
+		*openj9*)
+			VM="openj9"
+			JAVA_OPTS="${JAVA_OPTS} -Xgcpolicy:optthruput -Xgcthreads2 -Xenableexcessivegc -Xgc:excessiveGCratio=95"
+		;;
+
+		*j9*)
+			VM="openj9"
+			JAVA_OPTS="${JAVA_OPTS} -Xgcpolicy:optthruput -Xgcthreads2 -Xenableexcessivegc -Xgc:excessiveGCratio=95"
+		;;
+
+		*hotspot*)
+			VM="hotspot"
+			JAVA_OPTS="${JAVA_OPTS} -XX:+UseParallelGC -XX:ParallelGCThreads=2 -XX:GCTimeRatio=19"
+		;;
+
+		*)
+			echo -e "\n${0##*/}@${HOSTNAME}:$( date "+%Y.%m.%d.%H%M.%S" ): ERROR: Unknown JVM VM: '${VM}' in JAVA_ROOT: '${JAVA_ROOT}'! Expecting an OpenJ9 or Hotspot JVM.\n\nExiting...\n"
+			exit -1
+		;;
+	esac
+
+	for (( i=1; i <= ITERATE; i++ ))
 	do
-		for jvm in "${jdk_v}"/*
-		do
-			JAVA_HOME="${jvm}"
-			JAVA_OPTS=" -Xms2G -Xmx2G "
-			VM="$(basename "${jvm}")"
-			case "${VM}" in
-				*openj9*)
-					VM="openj9"
-					JAVA_OPTS="${JAVA_OPTS} -Xgcpolicy:optthruput -Xgcthreads2 -Xenableexcessivegc -Xgc:excessiveGCratio=95"
-				;;
-
-				*hotspot*)
-					VM="hotspot"
-					JAVA_OPTS="${JAVA_OPTS} -XX:+UseParallelGC -XX:ParallelGCThreads=2 -XX:GCTimeRatio=19"
-				;;
-			esac
-
-			for (( i=1; i <= ITERATE; i++ ))
-			do
-				make CPU_TYPE="${CPU_TYPE}" VM="${VM}" JAVA_HOME="${JAVA_HOME}" JAVA_OPTS="${JAVA_OPTS}"
-			done
-		done
+		make CPU_TYPE="${CPU_TYPE}" VM="${VM}" JAVA_HOME="${JAVA_HOME}" JAVA_OPTS="${JAVA_OPTS}"
 	done
+
 	popd || exit 1
 done
+
+echo -e "\n${0##*/}@${HOSTNAME}:$( date "+%Y.%m.%d.%H%M.%S" ): End (RC: '$?').\n\n"
